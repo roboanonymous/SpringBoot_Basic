012d454af88b77802094599852726442a4e60704
roboanonymous <jsamyak591@gmail.com>
2023-01-19 17:42:46 +0530

    first commit

---
+++ src/test/java/com/example/Gradle_spring/GradleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gradle_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
