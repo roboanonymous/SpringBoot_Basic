4c1045371269d1a6eb93e786e0f8b862e88f7a9e
roboanonymous <jsamyak591@gmail.com>
2023-01-20 18:45:02 +0530

    first

--- src/main/java/com/example/Gradle_spring/GradleSpringApplication.java
+++ src/main/java/com/example/Gradle_spring/GradleSpringApplication.java
@@ -3,13 +3,17 @@ package com.example.Gradle_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GradleSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GradleSpringApplication.class, args);
+		ConfigurableApplicationContext context =  SpringApplication.run(GradleSpringApplication.class, args);
+
+		Alien a = context.getBean(Alien.class);
+		a.show();
 	}
 
 }
