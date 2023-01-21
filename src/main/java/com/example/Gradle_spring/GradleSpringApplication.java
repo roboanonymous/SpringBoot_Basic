576e0cb18d498f20629d363609adca8fadf62387
roboanonymous <jsamyak591@gmail.com>
2023-01-21 18:57:07 +0530

    first

--- src/main/java/com/example/Gradle_spring/GradleSpringApplication.java
+++ src/main/java/com/example/Gradle_spring/GradleSpringApplication.java
@@ -4,16 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GradleSpringApplication {
-
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context =  SpringApplication.run(GradleSpringApplication.class, args);
-
-		Alien a = context.getBean(Alien.class);
-		a.show();
+	@RequestMapping("home")
+	public String home()
+	{
+		System.out.println("Hi");
+		return "Home.jsp";
 	}
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringApplication.class, args);
 
+	}
 }
---
+++ src/main/webapp/Home.jsp
@@ -0,0 +1,6 @@
+<html>
+<head><title>First JSP</title></head>
+<body>
+  Hello
+</body>
+</html>
